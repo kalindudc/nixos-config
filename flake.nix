7026c9775712c8d7d4eb87ad4204506ae1f2cad9
Kalindu De Costa <kalinduk.decosta@gmail.com>
2025-01-15 01:53:15 -0500

    Use home manager

--- flake.nix
+++ flake.nix
@@ -2,14 +2,26 @@
   description = "@kalindudc nixos-config";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    # https://nix-community.github.io/home-manager/index.xhtml#sec-flakes-nixos-module
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, ...}: {
+  outputs = inputs@{ self, nixpkgs, home-manager, ...}: {
     nixosConfigurations.beagle = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
       modules = [ 
         ./host/base/configuration.nix
         ./host/beagle/configuration.nix 
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.kalindu = import ./home.nix;
+        }
       ];
     };
   };
