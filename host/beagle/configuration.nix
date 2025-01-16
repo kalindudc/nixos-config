b2fe7fae21be30eb8f37f4ae5caa0087863ec726
Kalindu De Costa <kalinduk.decosta@gmail.com>
2025-01-16 00:00:14 -0500

    Configure sddm theme

--- host/beagle/configuration.nix
+++ host/beagle/configuration.nix
@@ -1,5 +1,11 @@
 { config, pkgs, ... }:
 
+let
+  screen = {
+    width = "1920";
+    height = "1200";
+  };
+in
 {
   imports = [
     ./hardware-configuration.nix
@@ -12,7 +18,7 @@
 
   services.displayManager.sddm = {
     enable = true;
-    theme = "breeze";
+    theme = "sddm-astronaut-theme";
     wayland = {
       enable = true;
       compositor = "weston";
@@ -43,9 +49,20 @@
     neovim
     vscode
     wget
+    (
+      sddm-astronaut.override {
+        themeConfig = {
+          Background = "${../../theme/wallpaper/lock_screen_wallpaper.jpg}";
+          FormPosition = "left";
+          ScreenWidth = "${screen.width}";
+          ScreenHeight = "${screen.height}";
+          MainColor="#343746";
+          AccentColor="#F8F8F2";
+          placeholderColor="#99999b";
+        };
+      }
+    )
   ];
-
-
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   # tl;dr: just don't change it
