48d608c0ee0909c8559649b0ace87d696487f0fc
Kalindu De Costa <kalinduk.decosta@gmail.com>
2025-01-18 11:43:28 -0500

    Fail setup_pi4.sh if not run from a sudo shell

--- script/setup_rpi4.sh
+++ script/setup_rpi4.sh
@@ -5,6 +5,11 @@
 #
 # This script will use an initial pre-built config to for the inital upgrade
 # and then will prompt for a device to apply the final configuration
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root."
+  echo "Start a sudo shel and re-run this script: sudo -i"
+  exit 1
+fi
 
 echo "Setting up Raspberry Pi 4 with NixOS"
 echo "====================================="
@@ -12,7 +17,7 @@ echo ""
 
 echo "Updating the firmware..."
 echo " "
-sudo nix-shell -p raspberrypi-eeprom --run '
+nix-shell -p raspberrypi-eeprom --run '
 mount /dev/disk/by-label/FIRMWARE /mnt
 BOOTFS=/mnt FIRMWARE_RELEASE_STATUS=stable rpi-eeprom-update -d -a
 '
@@ -22,13 +27,13 @@ echo " "
 
 echo "Pulling default configuration..."
 echo " "
-sudo curl -fSL --progress-bar https://raw.githubusercontent.com/kalindudc/nixos-config/main/host/base/rpi4/init.nix -o /etc/nixos/configuration.nix
+curl -fSL --progress-bar https://raw.githubusercontent.com/kalindudc/nixos-config/main/host/base/rpi4/init.nix -o /etc/nixos/configuration.nix
 echo " "
 echo "Default configuration pulled successfully!"
 
 echo "Rebuilding the system..."
 echo " "
-sudo nixos-rebuild switch
+nixos-rebuild switch --upgrade
 echo " "
 echo "System rebuilt successfully!"
 
@@ -38,7 +43,7 @@ git clone https://github.com/kalindudc/nixos-config.git
 
 # get device host from user input
 read -p "Enter the device host: " device_host
-nixos-rebuild switch --use-remote-sudo --flake "./nixos-config#$device_host"
+nixos-rebuild switch --flake "./nixos-config#$device_host"
 
 echo " "
 echo "Setup complete! Remember to set a password for your user."
