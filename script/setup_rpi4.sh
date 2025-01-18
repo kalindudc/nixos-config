dc2b8269ead60823eeebd067b1531bd13a186cde
Kalindu De Costa <kalinduk.decosta@gmail.com>
2025-01-18 11:13:32 -0500

    Add some debug outputs to setup_rpi4.sh

--- script/setup_rpi4.sh
+++ script/setup_rpi4.sh
@@ -6,17 +6,39 @@
 # This script will use an initial pre-built config to for the inital upgrade
 # and then will prompt for a device to apply the final configuration
 
+echo "Setting up Raspberry Pi 4 with NixOS"
+echo "====================================="
+echo ""
+
+echo "Updating the firmware..."
+echo " "
 sudo nix-shell -p raspberrypi-eeprom --run '
 mount /dev/disk/by-label/FIRMWARE /mnt
 BOOTFS=/mnt FIRMWARE_RELEASE_STATUS=stable rpi-eeprom-update -d -a
 '
+echo " "
+echo "Firmware updated successfully!"
+echo " "
 
-curl -fsSL https://raw.githubusercontent.com/kalindudc/nixos-config/main/host/base/rpi4/init.nix -o /etc/nixos/configuration.nix
+echo "Pulling default configuration..."
+echo " "
+sudo curl -fsSL https://raw.githubusercontent.com/kalindudc/nixos-config/main/host/base/rpi4/init.nix -o /etc/nixos/configuration.nix
+echo " "
+echo "Default configuration pulled successfully!"
+
+echo "Rebuilding the system..."
+echo " "
 sudo nixos-rebuild switch
+echo " "
+echo "System rebuilt successfully!"
 
+echo "Setting up nixos-config..."
+echo " "
 git clone https://github.com/kalindudc/nixos-config.git
 
 # get device host from user input
 read -p "Enter the device host: " device_host
-
 sudo nixos-rebuild switch --flake "nixos-config#$device_host"
+
+echo " "
+echo "Setup complete!"
