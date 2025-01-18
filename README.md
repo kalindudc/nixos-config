089c87a29defe321c447f667c053eabf2c10872f
Kalindu De Costa <kalinduk.decosta@gmail.com>
2025-01-18 12:05:44 -0500

    Disable root user as last step

--- README.md
+++ README.md
@@ -10,5 +10,6 @@ Have an AARCH64 image flashed to an SD-Card and have the Pi boot from it.
 
 #### Setup
 ```
+sudo -i
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/kalindudc/nixos-config/main/script/setup_rpi4.sh)"
 ```
--- script/setup_rpi4.sh
+++ script/setup_rpi4.sh
@@ -11,39 +11,49 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-echo "Setting up Raspberry Pi 4 with NixOS"
-echo "====================================="
+# Define color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo "${GREEN}Setting up Raspberry Pi 4 with NixOS${NC}"
+echo "${GREEN}=====================================${NC}"
 echo ""
 
-echo "Updating the firmware..."
+echo "${GREEN}Updating the firmware...${NC}"
 echo " "
 nix-shell -p raspberrypi-eeprom --run '
 mount /dev/disk/by-label/FIRMWARE /mnt
 BOOTFS=/mnt FIRMWARE_RELEASE_STATUS=stable rpi-eeprom-update -d -a
 '
 echo " "
-echo "Firmware updated successfully!"
+echo "${GREEN}Firmware updated successfully!${NC}"
 echo " "
 
-echo "Pulling default configuration..."
+echo "${GREEN}Pulling default configuration...${NC}"
 echo " "
 curl -fSL --progress-bar https://raw.githubusercontent.com/kalindudc/nixos-config/main/host/base/rpi4/init.nix -o /etc/nixos/configuration.nix
+echo "${GREEN}Default configuration pulled successfully!${NC}"
 echo " "
-echo "Default configuration pulled successfully!"
 
-echo "Rebuilding the system..."
+echo "${GREEN}Rebuilding the system...${NC}"
 echo " "
 nixos-rebuild switch --upgrade
+echo "${GREEN}System rebuilt successfully!${NC}"
 echo " "
-echo "System rebuilt successfully!"
 
-echo "Setting up nixos-config..."
+echo "${GREEN}Setting up nixos-config...${NC}"
 echo " "
 git clone https://github.com/kalindudc/nixos-config.git
 
 # get device host from user input
-read -p "Enter the device host: " device_host
-nixos-rebuild switch --flake "./nixos-config#$device_host"
+echo " "
+read -p "${RED}Enter the device host: ${NC}" device_host
+echo " "
+
+nixos-install switch --flake "./nixos-config#$device_host"
 
 echo " "
-echo "Setup complete! Remember to set a password for your user."
+echo "${GREEN}Setup complete! Remember to set a password for your user and the root.${NC}"
+echo "  > passwd root"
+echo "  > passwd <your-username>"
